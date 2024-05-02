0e60e3256a08e3713c001f0d85a00d3f841b059c
Gabriel Fonseca <gabriel.fonsecadev@gmail.com>
2024-05-01 21:16:17 -0300

    lint

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,31 +1,30 @@
 return {
   {
     "stevearc/conform.nvim",
-     event = 'BufWritePre', -- uncomment for format on save
+    event = 'BufWritePre',  -- uncomment for format on save
     config = function()
       require "configs.conform"
     end,
   },
 
   -- These are some examples, uncomment them if you want to see them work!
- {
-   "neovim/nvim-lspconfig",
+  {
+    "neovim/nvim-lspconfig",
     config = function()
       require("nvchad.configs.lspconfig").defaults()
-       require "configs.lspconfig"
-     
-     end,
-   },
-  
-   {
-   	"williamboman/mason.nvim",
- 	opts = {
-   		ensure_installed = {
-   			"lua-language-server", "stylua",
-   			"html-lsp", "css-lsp" , "prettier", "rust-analyzer"
-   		},
-   	},
-   },
+      require "configs.lspconfig"
+    end,
+  },
+
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua-language-server", "stylua",
+        "html-lsp", "css-lsp", "prettier", "rust-analyzer"
+      },
+    },
+  },
   --
   -- {
   -- 	"nvim-treesitter/nvim-treesitter",
